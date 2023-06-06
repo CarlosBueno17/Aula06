439e8f0b98a2184fb4dba8aa3c99feb049c35bb7
CarlosBueno17 <135742315+CarlosBueno17@users.noreply.github.com>
2023-06-06 17:06:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("Digite de 1 a 10");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero > 10)
+            {
+                Console.WriteLine("Digite um número válido");
+                return;
+            }
+            Console.WriteLine("FIM DO PROGRAMA");
+            Console.ReadKey();
+        }
+    }
+}
